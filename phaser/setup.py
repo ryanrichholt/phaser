637b6b21ee5458780a36f07dcc458a539724755e
Ryan Richholt <ryanrichholt@users.noreply.github.com>
2017-08-28 14:42:42 -0700

    Updated to reflect name change in read_variant_map.pyx

--- phaser/setup.py
+++ phaser/setup.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 
 setup(
   name = 'phASER Read Variant Mapper',
-  ext_modules = cythonize("read_variant_map.py"),
+  ext_modules = cythonize("read_variant_map.pyx"),
 )
