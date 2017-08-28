61cb0589a1c892eed0e60a3245214911b44d23fd
Ryan Richholt <rrichholt@tgen.org>
2017-08-28 12:09:38 -0700

    Added a setup.py for the entire project, this installs all 3 packages with executables: phaser, phaser-annotate, and phaser-ge-ae

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup, Extension
+
+
+setup(
+  name = 'phaser',
+  description = 'phASER Read Variant Mapper',
+  packages = ['phaser', 'phaser_annotate', 'phaser_gene_ae'],
+  setup_requires=[
+        # Setuptools 18.0 properly handles Cython extensions.
+        'setuptools>=18.0',
+	'Cython',
+	'scipy',
+    ],
+
+  entry_points = {
+    'console_scripts': [
+        'phaser = phaser.phaser:main',
+        'phaser-annotate = phaser_annotate.phaser_annotate:main',
+        'phaser-gene-ae = phaser_gene_ae.phaser_gene_ae:main',
+    ]
+  },
+
+  ext_modules=[
+        Extension(
+            'mylib',
+            sources=['phaser/read_variant_map.pyx'],
+        ),
+    ],
+)
